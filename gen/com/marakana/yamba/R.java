fe7476ba595dc3b1894e8bfccc9ad2843e785436
Geraldo Guiesi Junior <geraldo.guiesi@gmail.com>
2011-08-09 19:41:08 -0300

    included text count pg 73

--- gen/com/marakana/yamba/R.java
+++ gen/com/marakana/yamba/R.java
@@ -14,8 +14,9 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int buttonUpdate=0x7f050001;
-        public static final int editText=0x7f050000;
+        public static final int buttonUpdate=0x7f050002;
+        public static final int editText=0x7f050001;
+        public static final int textCount=0x7f050000;
     }
     public static final class layout {
         public static final int status=0x7f030000;
--- src/com/marakana/yamba/StatusActivity.java
+++ src/com/marakana/yamba/StatusActivity.java
@@ -1,39 +1,103 @@
 package com.marakana.yamba;
 
 import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class StatusActivity extends Activity {
-	private static final String TAG = StatusActivity.class.toString();
+public class StatusActivity extends Activity implements OnClickListener, TextWatcher {
+	private static final String TAG = "StatusActivity";
 	EditText editText;
 	Button updateButton;
 	Twitter twitter;
-	
+	TextView textCount;
+
 	/** Called when the activity is first created. */
-    @SuppressWarnings("deprecation")
+	@SuppressWarnings("deprecation")
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.status);
-        
-        //Find views
-        editText = (EditText) findViewById(R.id.editText);
-        updateButton = (Button) findViewById(R.id.buttonUpdate);
-        
-        updateButton.setOnClickListener((OnClickListener) this);
-        
-        twitter = new Twitter("student", "password");
-        twitter.setAPIRootUrl("http://yamba.marakana.com/api");
-    }
-    
-    public void onClick(View v) {
-    	twitter.setStatus(editText.getText().toString());
-    	Log.d(TAG, "onClicked");
-    }
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.status);
+
+		// Find views
+		editText = (EditText) findViewById(R.id.editText);
+		updateButton = (Button) findViewById(R.id.buttonUpdate);
+		updateButton.setOnClickListener(this);
+		
+		textCount = (TextView) findViewById(R.id.textCount);
+		textCount.setText(Integer.toString(140));
+		textCount.setTextColor(Color.GREEN);
+		editText.addTextChangedListener(this);
+
+		twitter = new Twitter("student", "password");
+		twitter.setAPIRootUrl("http://yamba.marakana.com/api");
+	}
+
+	class PostToTwitter extends AsyncTask<String, Integer, String> {
+		// Called to initiate the background activity
+		@Override
+		protected String doInBackground(String... statuses) {
+			try {
+				Twitter.Status status = twitter.updateStatus(statuses[0]);
+				return status.text;
+			} catch (TwitterException e) {
+				Log.e(TAG, e.toString());
+				e.printStackTrace();
+				return "Failed to Post";
+			}
+		}
+		
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			// TODO Auto-generated method stub
+			super.onProgressUpdate(values);
+		}
+		
+		@Override
+		protected void onPostExecute(String result) {
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+		}
+	}
+
+	public void onClick(View v) {
+		String status = editText.getText().toString();
+		new PostToTwitter().execute(status);
+		Log.d(TAG, "onClicked");
+	}
+
+	@Override
+	public void afterTextChanged(Editable statusText) {
+		int count = 140 - statusText.length();
+		textCount.setText(Integer.toString(count));
+		textCount.setTextColor(Color.GREEN);
+		if (count < 10) {
+			textCount.setTextColor(Color.YELLOW);
+		}
+		if (count < 0) {
+			textCount.setTextColor(Color.RED);
+		}
+	}
+
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {
+		
+	}
+
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+		// TODO Auto-generated method stub
+		
+	}
 }
